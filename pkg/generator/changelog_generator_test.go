9f4e8b7851489ecd7ca3eb05fe0c90cff0ff1200
Kimmo Saari <kimmo.saari@worddive.com>
2020-11-09 13:30:00 +0200

    feat: added missing types and re-ordered Change log types order

--- pkg/generator/changelog_generator_test.go
+++ pkg/generator/changelog_generator_test.go
@@ -14,8 +14,11 @@ func TestDefaultGenerator(t *testing.T) {
 		{},
 		{SHA: "123456789", Type: "feat", Scope: "app", Message: "commit message"},
 		{SHA: "abcd", Type: "fix", Scope: "", Message: "commit message"},
+		{SHA: "87654321", Type: "ci", Scope: "", Message: "commit message"},
+		{SHA: "43218765", Type: "build", Scope: "", Message: "commit message"},
 		{SHA: "12345678", Type: "yolo", Scope: "swag", Message: "commit message"},
 		{SHA: "12345678", Type: "chore", Scope: "", Message: "commit message", Raw: []string{"", "BREAKING CHANGE: test"}, Change: &semrel.Change{Major: true}},
+		{SHA: "12345679", Type: "chore!", Scope: "user", Message: "another commit message", Raw: []string{"another commit message", "changed ID int into UUID"}, Change: &semrel.Change{Major: true}},
 		{SHA: "stop", Type: "chore", Scope: "", Message: "not included"},
 	}
 	changelogConfig.LatestRelease = &semrel.Release{SHA: "stop"}
@@ -25,7 +28,9 @@ func TestDefaultGenerator(t *testing.T) {
 	if !strings.Contains(changelog, "* **app:** commit message (12345678)") ||
 		!strings.Contains(changelog, "* commit message (abcd)") ||
 		!strings.Contains(changelog, "#### yolo") ||
-		!strings.Contains(changelog, "```BREAKING CHANGE: test\n```") ||
+		!strings.Contains(changelog, "#### Build") ||
+		!strings.Contains(changelog, "#### CI") ||
+		!strings.Contains(changelog, "```\nBREAKING CHANGE: test\n```") ||
 		strings.Contains(changelog, "not included") {
 		t.Fail()
 	}
